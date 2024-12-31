aad2b97cfb2022469c9f28a1490f5a267fb32812
Kushagra Bhagat <bhagatkushagra@gmail.com>
2024-12-31 15:23:48 +0530

    Added validation for Post Mapping and improved exception handling

--- src/main/java/com/kushagra/rest/restful_web_services/users/User.java
+++ src/main/java/com/kushagra/rest/restful_web_services/users/User.java
@@ -1,5 +1,7 @@
 package com.kushagra.rest.restful_web_services.users;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class User {
     private int id;
+
+    @Size(min = 2, message = "Name should have at least two characters")
     private String name;
+
+    @Past(message = "Birthdate should be in the past")
     private LocalDateTime dob;
 }
